bc934467e85bf53cdd69c9d3b75191715844204a
bsluther <briansluther@gmail.com>
2022-11-14 14:10:57 -0700

    Initialized and got Tailwind working.

---
+++ apps/web/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Web from "."
+import "../styles/globals.css"
+
+
+const App = () => {
+  return <Web />
+}
+
+export default App
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "ui";
+import { Button } from 'ui'
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className='bg-pink-400'>Web</h1>
       <Button />
     </div>
   );
