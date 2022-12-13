7b4e002b050e376010772e2c9c3ed668d7bf8ed9
bsluther <briansluther@gmail.com>
2022-12-12 19:28:42 -0700

    ...

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,7 @@ const withTM = require('next-transpile-modules')(['ui', 'measure-ts', 'ahp-0', '
 
 module.exports = withTM({
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 });
