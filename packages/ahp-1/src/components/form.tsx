1450af87b6294a9dfe9da876fd09f90000d71e44
bsluther <briansluther@gmail.com>
2022-12-19 14:15:53 -0700

    ...

--- packages/ahp-1/src/components/form.tsx
+++ packages/ahp-1/src/components/form.tsx
@@ -4,8 +4,8 @@ import { CheckCircleSvg } from 'ui'
 const button = cva(['w-10 h-10'], {
   variants: {
     valid: {
-      true: 'text-neutral-100 hover:fill-green-400/50 cursor-pointer',
-      false: 'text-neutral-300 hover:fill-red-400/25 cursor-pointer'
+      true: 'text-neutral-900 sm:text-neutral-100 hover:fill-green-400/50 cursor-pointer',
+      false: 'text-neutral-500 sm:text-neutral-300 hover:fill-red-400/25 cursor-pointer'
     }
   }
 })
@@ -23,7 +23,7 @@ export const Form = ({ title, description, heading, updateTitle, updateDescripti
 
   return (
     <form
-      className='w-max max-w-full flex flex-col items-center bg-neutral-500 p-8 rounded-lg space-y-8'
+      className='w-max max-w-full flex flex-col items-center sm:bg-neutral-500 p-8 rounded-lg space-y-8'
       onSubmit={e => {
         e.preventDefault()
         if (isValid) {
@@ -31,8 +31,8 @@ export const Form = ({ title, description, heading, updateTitle, updateDescripti
         }
       }}
     >
-      <h1 className='text-black text-lg sm:text-xl'>{heading}</h1>
-      <div className='flex items-center space-x-2'>
+      <h1 className='text-black text-xl underline'>{heading}</h1>
+      <div className='flex flex-col sm:flex-row items-center gap-2'>
         <label 
           className='text-black'
           htmlFor='title'
@@ -50,7 +50,7 @@ export const Form = ({ title, description, heading, updateTitle, updateDescripti
           }}
         />
       </div>
-      <div className='flex items-center space-x-2'>
+      <div className='flex flex-col sm:flex-row items-center gap-2'>
         <label 
           className='text-black'
           htmlFor='description'
