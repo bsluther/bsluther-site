dcece82e3a3814bfec8e8bfaed4cd3a45733be41
bsluther <briansluther@gmail.com>
2023-01-04 18:05:48 -0700

    Updated blog posts, worked on mdx styling, blog layout

--- apps/web/src/components/blog/blogLayout.tsx
+++ apps/web/src/components/blog/blogLayout.tsx
@@ -3,25 +3,38 @@ import Link from 'next/link'
 
 const BlogNav = () => {
   return (
-    <nav className='flex flex-col w-48'>
-      <Link href='my-mdx-page'>My MDX Page</Link>
-      <Link href='the-abstract-ecosystem'>The Abstract Ecosystem</Link>
+    <nav className='flex-col hidden md:flex w-max px-4 bg-neutral-500 text-neutral-300 pt-4'>
+      <Link href='gainzville-the-big-picture'>Gainzville: The Big Picture</Link>
+      {/* <Link href='the-abstract-ecosystem'>The Abstract Ecosystem</Link> */}
     </nav>
   )
 }
 
 const components = {
-  h1: (props: any) => <p {...props} className='text-2xl' />
+  h1: (props: any) => <h1 {...props} className='text-2xl underline' />,
+  h3: (props: any) => <h1 {...props} className='text-xl underline' />,
+  code: (props: any) => {
+    if (props.className === 'hljs language-js') {
+      return <code {...props} />
+    }
+    return <code {...props} className='font-bold' />
+  },
+  pre: (props: any) => <pre className='' {...props} />,
+  p: (props: any) => <p {...props} className='' />,
+  li: (props: any) => <li {...props} className='text-blue-800 pl-2'>- {props.children}</li>,
+  blockquote: (props: any) => <blockquote className='ml-8 px-2 bg-red-800 text-neutral-400' {...props} />
 }
 
 export const BlogLayout = ({ children }: { children: JSX.Element }) => {
 
   return (
-    <div className='w-full h-full flex'>
-      <div className='grow h-full'>
-        <MDXProvider components={components}>
-          {children}
-        </MDXProvider>
+    <div className='w-full flex'>
+      <div className='max-w-full grow flex justify-center'>
+        <div className='w-full md:max-w-3xl p-4 space-y-4'>
+          <MDXProvider components={components}>
+            {children}
+          </MDXProvider>
+        </div>
       </div>
       <BlogNav />
     </div>
--- apps/web/src/pages/blog.tsx
+++ apps/web/src/pages/blog.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link'
+
 const Blog = () => {
 
   return (
-    <div className=''>Blog...</div>
+    <p className='p-4 flex flex-col space-y-4'>
+      <span>
+        So far I have one post:
+      </span>
+      <>
+        <Link 
+          className='font-semibold underline'
+          href='/blog/gainzville-the-big-picture'
+        >Gainzville: The Big Picture</Link>
+      </>
+    </p>
+
   )
 }
 
